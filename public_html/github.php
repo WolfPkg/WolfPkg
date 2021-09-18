fec961684ca2ed0137a6f2a243ca6a55ac684b64
Tino Didriksen <mail@tinodidriksen.com>
2021-09-18 10:41:00 +0200

    WIP

--- public_html/github.php
+++ public_html/github.php
@@ -1,5 +1,10 @@
 <?php
 
+$head = apache_request_headers();
+if (empty($head['X-GitHub-Event'])) {
+	return;
+}
+
 $_GET = $_GET ?? [];
 $_POST = $_POST ?? [];
 
@@ -7,5 +12,5 @@ if (!empty($_POST['payload'])) {
     $_POST['payload'] = json_decode($_POST['payload'], true);
 }
 
-$out = var_export(['HEAD' => apache_request_headers(), 'GET' => $_GET, 'POST' => $_POST], true);
+$out = var_export(['HEAD' => $head, 'GET' => $_GET, 'POST' => $_POST], true);
 file_put_contents(__DIR__.'/github-'.date('YmdHis').'-'.sha1($out).'.txt', $out);
